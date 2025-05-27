0cc1b097b0621e4bdebd3d30d7784701bfa703f2
jamilhasan3301 <hasan15-5222@diu.edu.bd>
2025-05-27 17:31:07 +0600

    basic design

--- fullstakdynamic/resources/views/welcome.blade.php
+++ fullstakdynamic/resources/views/welcome.blade.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>jamil hasan portfolio</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>welcome</h1>
+    <div>
+        <nav>
+            <div class="logo">jamil</div>
+            <p>
+                home 
+                about
+                service
+            </p>
+        </nav>
+    </div>
+    <div>
+        <h2>Hello, I'm<br>Jamil Hasan</h2>
+        <div class="stats">
+            <div>15%</div>
+            <div>250+</div>
+            <div>58</div>
+        </div>
+    </div>
+    <div>
+        <div><img src="{{asset('image\196415165.jpg')}}" alt="photo"></div>
 </body>
 </html>
